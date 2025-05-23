b2a310370a5e869a389ebee323c4ef7a46177fd5
Robert Winder <robert@zoolylabs.com>
2025-05-23 13:04:46 -0600

    Add GitHub push script

---
+++ push-to-github.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Replace YOUR_USERNAME with your GitHub username
+
+git remote add origin https://github.com/YOUR_USERNAME/market_research_survey.git
+git branch -M main
+git push -u origin main
